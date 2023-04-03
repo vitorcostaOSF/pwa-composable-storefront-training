f634fadcc04625d74ab7b55271b9f7094a011e23
vitorcostaOSF <vitor.costa@osf.digital>
2023-04-03 16:01:16 -0300

    Day 2 - Exercise 1

---
+++ app/pages/content-search/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import fetch from 'cross-fetch'
+
+import { List, ListItem } from '@chakra-ui/react'
+import Link from '../../components/link'
+
+const ContentSearch = ({ contentResult }) => {
+    if (!contentResult) {
+        return <div>Loading...</div>
+    }
+
+    const { hits = [] } = contentResult
+    return (
+        <div>
+            {hits.length ? (
+                <List>
+                    {hits.map(({ id, name }) => (
+                        <Link key={id} to={`/content/${id}`}>
+                            <ListItem>{name}</ListItem>
+                        </Link>
+                    ))}
+                </List>
+            ) : (
+                <div>No Content Items Found!</div>
+            )}
+        </div>
+    )
+}
+
+ContentSearch.getProps = async () => {
+    let clientID = '264f0d69-a6b7-4001-a2df-20d025275702';
+    let siteID = 'RefArch';
+    let contentResult
+    //Make a call to the URL substituting Key Values from table
+    const res = await fetch(
+        `http://localhost:3000/mobify/proxy/ocapi/s/${siteID}/dw/shop/v20_2/content_search?q=about&client_id=${clientID}`
+    )
+    if (res.ok) {
+        contentResult = await res.json()
+    }
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(contentResult)
+    }
+    return { contentResult }
+}
+
+ContentSearch.getTemplateName = () => 'content-search'
+
+export default ContentSearch
